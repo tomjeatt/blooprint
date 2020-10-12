14a5b2f28e6c3330ee5513e749d7b7229b24642b
Thomas Jeatt <hello@tomjeatt.com>
2020-10-12 10:26:44 +0100

    Create editable form fields

--- src/components/Card/Card.svelte
+++ src/components/Card/Card.svelte
@@ -1,6 +1,39 @@
 <script>
   import data from "../../data/card.json";
+
+  const { text } = data;
+
+  let textField;
+
+  const makeEditable = () => {
+    textField.removeAttribute("readonly");
+    textField.focus();
+  };
+
+  const saveCard = (event) => {
+    event.preventDefault();
+
+    textField.setAttribute("readonly", true);
+  };
 </script>
 
-<h2>Card</h2>
-<p>{data.text}</p>
+<style>
+  .card {
+    border: 1px solid lightgray;
+    margin: 0 auto;
+    width: 400px;
+  }
+
+  textarea {
+    display: block;
+    width: 100%;
+  }
+</style>
+
+<div class="card">
+  <form on:submit={saveCard}>
+    <textarea value={text} bind:this={textField} readonly />
+    <button type="submit">✓</button>
+  </form>
+  <button on:click={makeEditable}>Edit</button>
+</div>
