dc18813e4354472a3ff26c1458633aebb3ad58cc
Darien Opperman <darien.opperman@kaluza.com>
2020-10-14 16:34:48 +0100

    Extract grid to it's own component

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,10 @@
 <script lang="ts">
     import data from './data/card.json';
-    import Row from './components/Row.svelte';
-    import type { RowData } from './main';
-    const rowData: Array<RowData> = data;
+    import Grid from './components/Grid.svelte';
 </script>
 
 <main>
     <h1>Blooprint</h1>
     <p>A service blueprinting application.</p>
-    <button>New Column</button>
-    <button>New Row</button>
-    <div>
-        {#each rowData as row, i}
-            <Row cards={row} rowNumber={i} />
-        {/each}
-    </div>
+    <Grid rowData={data} />
 </main>
-
-<style>
-    button {
-        width: 10rem;
-    }
-</style>
---
+++ src/components/Grid.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import Row from './Row.svelte';
+    import type { RowData } from '../main';
+
+    export let rowData: Array<RowData>;
+</script>
+
+<div>
+    <button>New Column</button>
+    <button>New Row</button>
+    <div>
+        {#each rowData as row, i}
+            <Row cards={row} rowNumber={i} />
+        {/each}
+    </div>
+</div>
+
+<style>
+    button {
+        width: 10rem;
+    }
+</style>
