5c0414f4dca80939a34e0665b5b88d55461d4eaa
Darien Opperman <darien.opperman@kaluza.com>
2020-10-14 10:47:03 +0100

    Format existing files with prettier

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
-<script>
-  import Card from "./components/Card/Card.svelte";
+<script lang="ts">
+    import Card from './components/Card/Card.svelte';
 </script>
 
 <main>
-  <h1>Blooprint</h1>
-  <p>A service blueprinting application.</p>
-  <Card />
+    <h1>Blooprint</h1>
+    <p>A service blueprinting application.</p>
+    <Card />
 </main>
--- src/components/Card/Card.svelte
+++ src/components/Card/Card.svelte
@@ -1,45 +1,46 @@
 <script lang="ts">
-  import data from "../../data/card.json";
+    import data from '../../data/card.json';
 
-  const { text } = data;
+    const { text } = data;
 
-  let textField: HTMLElement;
+    let cards = [data];
+    let textField: HTMLElement;
 
-  const makeEditable: () => void = () => {
-    textField.removeAttribute("readonly");
-    textField.focus();
-  };
+    const makeEditable: () => void = () => {
+        textField.removeAttribute('readonly');
+        textField.focus();
+    };
 
-  const saveCard: (event: Event) => void = (event) => {
-    event.preventDefault();
+    const saveCard: (event: Event) => void = (event) => {
+        event.preventDefault();
 
-    textField.setAttribute("readonly", "true");
-  };
+        textField.setAttribute('readonly', 'true');
+    };
 </script>
 
-<style>
-  .card {
-    border: 1px solid lightgray;
-    margin: 0 auto;
-    width: 400px;
-  }
-
-  textarea {
-    display: block;
-    margin: 5px auto;
-    resize: none;
-    width: 94%;
-  }
-
-  textarea:read-only {
-    border: none;
-  }
-</style>
-
 <div class="card">
-  <form on:submit={saveCard}>
-    <textarea value={text} bind:this={textField} readonly />
-    <button type="submit">✓</button>
-  </form>
-  <button on:click={makeEditable}>Edit</button>
+    <form on:submit={saveCard}>
+        <textarea value={text} bind:this={textField} readonly />
+        <button type="submit">✓</button>
+    </form>
+    <button on:click={makeEditable}>Edit</button>
 </div>
+
+<style>
+    .card {
+        border: 1px solid lightgray;
+        margin: 0 auto;
+        width: 400px;
+    }
+
+    textarea {
+        display: block;
+        margin: 5px auto;
+        resize: none;
+        width: 94%;
+    }
+
+    textarea:read-only {
+        border: none;
+    }
+</style>
